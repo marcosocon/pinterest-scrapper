c97f2a08663e3fc6e2349507ab3eca8b2749a7f9
marcosocon <oconmarcos97@gmail.com>
2016-06-23 01:31:50 -0300

    added packages, server.js, and gitignore

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var cheerio = require('cheerio');
+var mongoose = require('mongoose');
+var path = require('path');
+var routes = require('./routes');
+var app = express();
+
+mongoose.connect('mongodb://localhost/pinterest-scrapper');
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', routes);
+
+app.listen(3000, function(){
+	console.log("Express server running at localhost:3000, enjoy!");
+});
