c91239172985bd6ce62c8c41e6b00f8328b6986b
marcosocon <oconmarcos97@gmail.com>
2016-06-23 01:49:31 -0300

    added mongoose model

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var Item = require('./models/item.model');
 
 router.get('/test', function(req,res){
 	res.send('hello from /api/test!');
 });
+
+module.exports = router;
