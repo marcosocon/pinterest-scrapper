726b13707b502f4e279515958b642f7efa670402
marcosocon <oconmarcos97@gmail.com>
2016-06-23 02:15:53 -0300

    added app.js and controller scaffold

---
+++ public/js/controller.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+		angular
+			.module('app')
+				.controller('MainCtrl', MainCtrl);
+				MainCtrl.$inject = ['$scope','$http'];
+				function MainCtrl($scope, $http){
+
+				}
+
+})();
